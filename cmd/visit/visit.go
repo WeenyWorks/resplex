40ae5b80494b5509c1b4062ea8a9b4f688a0ca98
chengjunxie <bbboson@gmail.com>
2021-09-04 16:55:44 +0800

    clean up visit.go

--- cmd/visit/visit.go
+++ cmd/visit/visit.go
@@ -3,24 +3,48 @@ package visit
 import (
 	"bufio"
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 
+	"github.com/WeenyWorks/resplex/lib/regheader"
 	"github.com/WeenyWorks/resplex/lib/visheader"
 	"github.com/spf13/cobra"
+	"github.com/xtaci/smux"
 )
 
-func entry(cmd *cobra.Command, args []string) {
-	conn, err := net.Dial("tcp", "127.0.0.1:9898")
-	if err != nil {
-		panic(err)
+type vistor struct{
+	proxyAddr string
+	proxyPort string
+	targetMachine string
+	targetPort int
+	conn net.Conn
+	stream *smux.Stream
+}
+
+func newVistor(pa string, pp string, tm string, tp int) *vistor {
+	return &vistor{
+		proxyAddr:     pa,
+		proxyPort:     pp,
+		targetMachine: tm,
+		targetPort:    tp,
 	}
-	defer conn.Close()
+}
+
+// visitor is a one-shot command thus we shouldn't retry any connection.
+func (v *vistor)connectProxy() error {
+	conn, err := net.Dial("tcp", fmt.Sprint(v.proxyAddr, ":", v.proxyPort))
+	v.conn = conn
+	return err
+}
+
+func(v *vistor)connectMachine() error {
 	vh := &visheader.VisHeader{
-		MachineID: "TESTMACHINEIDXCJ",
-		Port:      22,
+		MachineID: regheader.MachineID(v.targetMachine),
+		Port:      uint16(v.targetPort),
 	}
 	vhs, err := vh.Marshal()
 	if err != nil {
@@ -28,34 +52,55 @@ func entry(cmd *cobra.Command, args []string) {
 	}
 	lenBuf := make([]byte, 2)
 	binary.LittleEndian.PutUint16(lenBuf, uint16(len(vhs)))
-	n, err := conn.Write(lenBuf)
+	n, err := v.conn.Write(lenBuf)
 	if n != 2 {
 		panic("failed to write len")
 	}
 	log.Println(vhs)
-	n, err = conn.Write(vhs)
-	stdio := bufio.NewReadWriter(bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout))
-	go func() {
-		for {
-			_, err := io.Copy(conn, stdio)
-			if err != nil {
-				break
-			}
-		}
-	}()
-	for {
-		io.Copy(stdio, conn)
-		if err != nil {
-			break
-		}
+	n, err = v.conn.Write(vhs)
+	if err != nil {
+		return err
 	}
+	if n != len(vhs) {
+		return errors.New("Error: short write when writing visheader")
+	}
+	return nil
 }
 
+func(v vistor)serve() error {
+	stdio := bufio.NewReadWriter(bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout))
+	go func(){
+		_, err := io.Copy(v.conn, stdio)
+		if err != nil {
+			return
+		}
+	}()
+	_, err := io.Copy(stdio, v.conn)
+	return err
 
+}
 
+var targetMachine string
+var targetPort int
+var proxyAddr string
+var proxyPort string
+
+func entry(cmd *cobra.Command, args []string) {
+	v := newVistor(proxyAddr, proxyPort, targetMachine, targetPort)
+	v.connectProxy()
+	v.connectMachine()
+	v.serve()
+}
 
 var VisitCMD = &cobra.Command{
 	Use: "visit",
 	Short: "visit machine",
 	Run: entry,
+}
+
+func init() {
+	VisitCMD.PersistentFlags().StringVarP(&targetMachine, "machine", "m", "", "The machine ID that you want to visit")
+	VisitCMD.PersistentFlags().IntVarP(&targetPort, "service", "s", 22, "The target service that you want to visit.")
+	VisitCMD.PersistentFlags().StringVarP(&proxyAddr, "proxyaddr", "x", "127.0.0.1", "The proxy service address")
+	VisitCMD.PersistentFlags().StringVarP(&proxyPort, "proxyport", "p", "9898", "The proxy service port")
 }
